64ce20b8f0ad8c4804f6307ccd838eb0cc70ac4f
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2024-03-06 13:19:56 +0100

    ss

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfTheWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isWeekend = (day: DayOfTheWeek) => {
+  return day === DayOfTheWeek.Saturday || day === DayOfTheWeek.Sunday;
+};
+
+console.log(isWeekend(DayOfTheWeek.Saturday));
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,23 +1,32 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
-
-const page1 = {
-  title: 'The awesome page',
+type forBoth = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+const page1: forBoth = {
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page2: forBoth = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
